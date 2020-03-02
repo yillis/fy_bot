dd676a51237b69a8684bdb7cc4f12dfdc5bb852d
Miao Wang <424258384@qq.com>
2020-03-02 18:13:58 +0800

    almost finish except NLP session

--- awesome/util/const.py
+++
@@ -1,7 +0,0 @@
-import awesome.plugins.ask.data_source
-import awesome.plugins.update.data_source
-import awesome.util.repair_sqlite as sqlite
-
-TB_NAME = 'fybot.db'
-ASK_KEYWORD = awesome.plugins.ask.data_source.get_ask_keyword()
-UPDATE_KEYWORD = awesome.plugins.update.data_source.get_update_keyword()
